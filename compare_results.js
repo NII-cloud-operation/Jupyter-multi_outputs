3309b44c4db2fc51a3b40c3d0751900bdc2a2b8c
takashi maruoka <takashi@nii.ac.jp>
2016-01-27 02:31:20 +0000

    Add compare_result.js

---
+++ compare_results.js
@@ -0,0 +1,12 @@
+define([
+    'base/js/namespace',
+    'jquery',
+    'require'
+],   function(IPython, $, require) {
+    "use strict";
+
+    var compare_results = function() {
+    };
+
+    return { load_ipython_extension : compare_results };
+});
