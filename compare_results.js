36030a67b80c8fdf92aeab0b08a69f7c5541dd2c
takashi MARUOKA <takashi@nii.ac.jp>
2016-04-25 05:36:07 +0000

    Add CodeCell.prototype.execute from original

--- compare_results.js
+++ compare_results.js
@@ -1,8 +1,9 @@
 define([
     'base/js/namespace',
     'jquery',
-    'require'
-],   function(IPython, $, require) {
+    'require',
+    'notebook/js/codecell'
+],   function(IPython, $, require, codecell) {
     "use strict";
 
     var register_toolbar_menu = function() {
@@ -32,6 +33,39 @@ define([
 
     var compare_results = function() {
         register_toolbar_menu()
+
+        codecell.CodeCell.prototype.execute = function (stop_on_error) {
+            if (!this.kernel) {
+                console.log("Can't execute cell since kernel is not set.");
+                return;
+            }
+
+            if (stop_on_error === undefined) {
+                stop_on_error = true;
+            }
+
+            this.output_area.clear_output(false, true);
+            var old_msg_id = this.last_msg_id;
+            if (old_msg_id) {
+                this.kernel.clear_callbacks_for_msg(old_msg_id);
+                delete codecell.CodeCell.msg_cells[old_msg_id];
+                this.last_msg_id = null;
+            }
+            if (this.get_text().trim().length === 0) {
+                // nothing to do
+                this.set_input_prompt(null);
+                return;
+            }
+            this.set_input_prompt('*');
+            this.element.addClass("running");
+            var callbacks = this.get_callbacks();
+
+            this.last_msg_id = this.kernel.execute(this.get_text(), callbacks, {silent: false, store_history: true,
+                stop_on_error : stop_on_error});
+            codecell.CodeCell.msg_cells[this.last_msg_id] = this;
+            this.render();
+            this.events.trigger('execute.CodeCell', {cell: this});
+        };
     };
 
     return { load_ipython_extension : compare_results };
